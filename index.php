549c149f48e88c307a82ff17dcf9eb63469d5042
john <john@pocdigital.org>
2018-06-23 15:11:56 -0400

    piwik working

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
 <?php
-  require_once 'vendor/autoload.php';
-print __DIR__  . PHP_EOL;
-chdir('vendor/piwik/piwik');
-print getcwd() . PHP_EOL;
-print __DIR__  . PHP_EOL;
-//phpinfo();
-require_once 'vendor/piwik/piwik/index.php';
+header('Location: piwik/piwik/');
