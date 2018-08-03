308a14ecc0c3fabbb2c090d5e65ecf5a27390459
john <john@pocdigital.org>
2018-08-03 14:16:40 -0400

    added sql dump to repo

---
+++ config/matomo.sql
@@ -0,0 +1,547 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 02, 2018 at 08:37 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test1`
+--
+CREATE DATABASE IF NOT EXISTS `test1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `test1`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_access`
+--
+
+DROP TABLE IF EXISTS `matomo_access`;
+CREATE TABLE IF NOT EXISTS `matomo_access` (
+  `login` varchar(100) NOT NULL,
+  `idsite` int(10) unsigned NOT NULL,
+  `access` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`login`,`idsite`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_goal`
+--
+
+DROP TABLE IF EXISTS `matomo_goal`;
+CREATE TABLE IF NOT EXISTS `matomo_goal` (
+  `idsite` int(11) NOT NULL,
+  `idgoal` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `match_attribute` varchar(20) NOT NULL,
+  `pattern` varchar(255) NOT NULL,
+  `pattern_type` varchar(10) NOT NULL,
+  `case_sensitive` tinyint(4) NOT NULL,
+  `allow_multiple` tinyint(4) NOT NULL,
+  `revenue` float NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idsite`,`idgoal`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_logger_message`
+--
+
+DROP TABLE IF EXISTS `matomo_logger_message`;
+CREATE TABLE IF NOT EXISTS `matomo_logger_message` (
+  `idlogger_message` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tag` varchar(50) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT NULL,
+  `level` varchar(16) DEFAULT NULL,
+  `message` text,
+  PRIMARY KEY (`idlogger_message`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_log_action`
+--
+
+DROP TABLE IF EXISTS `matomo_log_action`;
+CREATE TABLE IF NOT EXISTS `matomo_log_action` (
+  `idaction` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `hash` int(10) unsigned NOT NULL,
+  `type` tinyint(3) unsigned DEFAULT NULL,
+  `url_prefix` tinyint(2) DEFAULT NULL,
+  PRIMARY KEY (`idaction`),
+  KEY `index_type_hash` (`type`,`hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_log_conversion`
+--
+
+DROP TABLE IF EXISTS `matomo_log_conversion`;
+CREATE TABLE IF NOT EXISTS `matomo_log_conversion` (
+  `idvisit` bigint(10) unsigned NOT NULL,
+  `idsite` int(10) unsigned NOT NULL,
+  `idvisitor` binary(8) NOT NULL,
+  `server_time` datetime NOT NULL,
+  `idaction_url` int(10) unsigned DEFAULT NULL,
+  `idlink_va` bigint(10) unsigned DEFAULT NULL,
+  `idgoal` int(10) NOT NULL,
+  `buster` int(10) unsigned NOT NULL,
+  `idorder` varchar(100) DEFAULT NULL,
+  `items` smallint(5) unsigned DEFAULT NULL,
+  `url` text NOT NULL,
+  `visitor_days_since_first` smallint(5) unsigned DEFAULT NULL,
+  `visitor_days_since_order` smallint(5) unsigned DEFAULT NULL,
+  `visitor_returning` tinyint(1) DEFAULT NULL,
+  `visitor_count_visits` int(11) unsigned NOT NULL,
+  `referer_keyword` varchar(255) DEFAULT NULL,
+  `referer_name` varchar(70) DEFAULT NULL,
+  `referer_type` tinyint(1) unsigned DEFAULT NULL,
+  `config_device_brand` varchar(100) DEFAULT NULL,
+  `config_device_model` varchar(100) DEFAULT NULL,
+  `config_device_type` tinyint(100) DEFAULT NULL,
+  `location_city` varchar(255) DEFAULT NULL,
+  `location_country` char(3) DEFAULT NULL,
+  `location_latitude` decimal(9,6) DEFAULT NULL,
+  `location_longitude` decimal(9,6) DEFAULT NULL,
+  `location_region` char(3) DEFAULT NULL,
+  `revenue` float DEFAULT NULL,
+  `revenue_discount` float DEFAULT NULL,
+  `revenue_shipping` float DEFAULT NULL,
+  `revenue_subtotal` float DEFAULT NULL,
+  `revenue_tax` float DEFAULT NULL,
+  `custom_var_k1` varchar(200) DEFAULT NULL,
+  `custom_var_v1` varchar(200) DEFAULT NULL,
+  `custom_var_k2` varchar(200) DEFAULT NULL,
+  `custom_var_v2` varchar(200) DEFAULT NULL,
+  `custom_var_k3` varchar(200) DEFAULT NULL,
+  `custom_var_v3` varchar(200) DEFAULT NULL,
+  `custom_var_k4` varchar(200) DEFAULT NULL,
+  `custom_var_v4` varchar(200) DEFAULT NULL,
+  `custom_var_k5` varchar(200) DEFAULT NULL,
+  `custom_var_v5` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`idvisit`,`idgoal`,`buster`),
+  UNIQUE KEY `unique_idsite_idorder` (`idsite`,`idorder`),
+  KEY `index_idsite_datetime` (`idsite`,`server_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_log_conversion_item`
+--
+
+DROP TABLE IF EXISTS `matomo_log_conversion_item`;
+CREATE TABLE IF NOT EXISTS `matomo_log_conversion_item` (
+  `idsite` int(10) unsigned NOT NULL,
+  `idvisitor` binary(8) NOT NULL,
+  `server_time` datetime NOT NULL,
+  `idvisit` bigint(10) unsigned NOT NULL,
+  `idorder` varchar(100) NOT NULL,
+  `idaction_sku` int(10) unsigned NOT NULL,
+  `idaction_name` int(10) unsigned NOT NULL,
+  `idaction_category` int(10) unsigned NOT NULL,
+  `idaction_category2` int(10) unsigned NOT NULL,
+  `idaction_category3` int(10) unsigned NOT NULL,
+  `idaction_category4` int(10) unsigned NOT NULL,
+  `idaction_category5` int(10) unsigned NOT NULL,
+  `price` float NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `deleted` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`idvisit`,`idorder`,`idaction_sku`),
+  KEY `index_idsite_servertime` (`idsite`,`server_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_log_link_visit_action`
+--
+
+DROP TABLE IF EXISTS `matomo_log_link_visit_action`;
+CREATE TABLE IF NOT EXISTS `matomo_log_link_visit_action` (
+  `idlink_va` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idsite` int(10) unsigned NOT NULL,
+  `idvisitor` binary(8) NOT NULL,
+  `idvisit` bigint(10) unsigned NOT NULL,
+  `idaction_url_ref` int(10) unsigned DEFAULT '0',
+  `idaction_name_ref` int(10) unsigned DEFAULT NULL,
+  `custom_float` float DEFAULT NULL,
+  `server_time` datetime NOT NULL,
+  `idpageview` char(6) DEFAULT NULL,
+  `interaction_position` smallint(5) unsigned DEFAULT NULL,
+  `idaction_name` int(10) unsigned DEFAULT NULL,
+  `idaction_url` int(10) unsigned DEFAULT NULL,
+  `time_spent_ref_action` int(10) unsigned DEFAULT NULL,
+  `idaction_event_action` int(10) unsigned DEFAULT NULL,
+  `idaction_event_category` int(10) unsigned DEFAULT NULL,
+  `idaction_content_interaction` int(10) unsigned DEFAULT NULL,
+  `idaction_content_name` int(10) unsigned DEFAULT NULL,
+  `idaction_content_piece` int(10) unsigned DEFAULT NULL,
+  `idaction_content_target` int(10) unsigned DEFAULT NULL,
+  `custom_var_k1` varchar(200) DEFAULT NULL,
+  `custom_var_v1` varchar(200) DEFAULT NULL,
+  `custom_var_k2` varchar(200) DEFAULT NULL,
+  `custom_var_v2` varchar(200) DEFAULT NULL,
+  `custom_var_k3` varchar(200) DEFAULT NULL,
+  `custom_var_v3` varchar(200) DEFAULT NULL,
+  `custom_var_k4` varchar(200) DEFAULT NULL,
+  `custom_var_v4` varchar(200) DEFAULT NULL,
+  `custom_var_k5` varchar(200) DEFAULT NULL,
+  `custom_var_v5` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`idlink_va`),
+  KEY `index_idvisit` (`idvisit`),
+  KEY `index_idsite_servertime` (`idsite`,`server_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_log_profiling`
+--
+
+DROP TABLE IF EXISTS `matomo_log_profiling`;
+CREATE TABLE IF NOT EXISTS `matomo_log_profiling` (
+  `query` text NOT NULL,
+  `count` int(10) unsigned DEFAULT NULL,
+  `sum_time_ms` float DEFAULT NULL,
+  UNIQUE KEY `query` (`query`(100))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_log_visit`
+--
+
+DROP TABLE IF EXISTS `matomo_log_visit`;
+CREATE TABLE IF NOT EXISTS `matomo_log_visit` (
+  `idvisit` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idsite` int(10) unsigned NOT NULL,
+  `idvisitor` binary(8) NOT NULL,
+  `visit_last_action_time` datetime NOT NULL,
+  `config_id` binary(8) NOT NULL,
+  `location_ip` varbinary(16) NOT NULL,
+  `user_id` varchar(200) DEFAULT NULL,
+  `visit_first_action_time` datetime NOT NULL,
+  `visit_goal_buyer` tinyint(1) DEFAULT NULL,
+  `visit_goal_converted` tinyint(1) DEFAULT NULL,
+  `visitor_days_since_first` smallint(5) unsigned DEFAULT NULL,
+  `visitor_days_since_order` smallint(5) unsigned DEFAULT NULL,
+  `visitor_returning` tinyint(1) DEFAULT NULL,
+  `visitor_count_visits` int(11) unsigned NOT NULL,
+  `visit_entry_idaction_name` int(10) unsigned DEFAULT NULL,
+  `visit_entry_idaction_url` int(11) unsigned DEFAULT NULL,
+  `visit_exit_idaction_name` int(10) unsigned DEFAULT NULL,
+  `visit_exit_idaction_url` int(10) unsigned DEFAULT '0',
+  `visit_total_actions` int(11) unsigned DEFAULT NULL,
+  `visit_total_interactions` smallint(5) unsigned DEFAULT '0',
+  `visit_total_searches` smallint(5) unsigned DEFAULT NULL,
+  `referer_keyword` varchar(255) DEFAULT NULL,
+  `referer_name` varchar(70) DEFAULT NULL,
+  `referer_type` tinyint(1) unsigned DEFAULT NULL,
+  `referer_url` text,
+  `location_browser_lang` varchar(20) DEFAULT NULL,
+  `config_browser_engine` varchar(10) DEFAULT NULL,
+  `config_browser_name` varchar(10) DEFAULT NULL,
+  `config_browser_version` varchar(20) DEFAULT NULL,
+  `config_device_brand` varchar(100) DEFAULT NULL,
+  `config_device_model` varchar(100) DEFAULT NULL,
+  `config_device_type` tinyint(100) DEFAULT NULL,
+  `config_os` char(3) DEFAULT NULL,
+  `config_os_version` varchar(100) DEFAULT NULL,
+  `visit_total_events` int(11) unsigned DEFAULT NULL,
+  `visitor_localtime` time DEFAULT NULL,
+  `visitor_days_since_last` smallint(5) unsigned DEFAULT NULL,
+  `config_resolution` varchar(18) DEFAULT NULL,
+  `config_cookie` tinyint(1) DEFAULT NULL,
+  `config_director` tinyint(1) DEFAULT NULL,
+  `config_flash` tinyint(1) DEFAULT NULL,
+  `config_gears` tinyint(1) DEFAULT NULL,
+  `config_java` tinyint(1) DEFAULT NULL,
+  `config_pdf` tinyint(1) DEFAULT NULL,
+  `config_quicktime` tinyint(1) DEFAULT NULL,
+  `config_realplayer` tinyint(1) DEFAULT NULL,
+  `config_silverlight` tinyint(1) DEFAULT NULL,
+  `config_windowsmedia` tinyint(1) DEFAULT NULL,
+  `visit_total_time` int(11) unsigned NOT NULL,
+  `location_city` varchar(255) DEFAULT NULL,
+  `location_country` char(3) DEFAULT NULL,
+  `location_latitude` decimal(9,6) DEFAULT NULL,
+  `location_longitude` decimal(9,6) DEFAULT NULL,
+  `location_region` char(3) DEFAULT NULL,
+  `custom_var_k1` varchar(200) DEFAULT NULL,
+  `custom_var_v1` varchar(200) DEFAULT NULL,
+  `custom_var_k2` varchar(200) DEFAULT NULL,
+  `custom_var_v2` varchar(200) DEFAULT NULL,
+  `custom_var_k3` varchar(200) DEFAULT NULL,
+  `custom_var_v3` varchar(200) DEFAULT NULL,
+  `custom_var_k4` varchar(200) DEFAULT NULL,
+  `custom_var_v4` varchar(200) DEFAULT NULL,
+  `custom_var_k5` varchar(200) DEFAULT NULL,
+  `custom_var_v5` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`idvisit`),
+  KEY `index_idsite_config_datetime` (`idsite`,`config_id`,`visit_last_action_time`),
+  KEY `index_idsite_datetime` (`idsite`,`visit_last_action_time`),
+  KEY `index_idsite_idvisitor` (`idsite`,`idvisitor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_option`
+--
+
+DROP TABLE IF EXISTS `matomo_option`;
+CREATE TABLE IF NOT EXISTS `matomo_option` (
+  `option_name` varchar(255) NOT NULL,
+  `option_value` longtext NOT NULL,
+  `autoload` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`option_name`),
+  KEY `autoload` (`autoload`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_plugin_setting`
+--
+
+DROP TABLE IF EXISTS `matomo_plugin_setting`;
+CREATE TABLE IF NOT EXISTS `matomo_plugin_setting` (
+  `plugin_name` varchar(60) NOT NULL,
+  `setting_name` varchar(255) NOT NULL,
+  `setting_value` longtext NOT NULL,
+  `json_encoded` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `user_login` varchar(100) NOT NULL DEFAULT '',
+  KEY `plugin_name` (`plugin_name`,`user_login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_privacy_logdata_anonymizations`
+--
+
+DROP TABLE IF EXISTS `matomo_privacy_logdata_anonymizations`;
+CREATE TABLE IF NOT EXISTS `matomo_privacy_logdata_anonymizations` (
+  `idlogdata_anonymization` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `idsites` text,
+  `date_start` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `anonymize_ip` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `anonymize_location` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `anonymize_userid` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `unset_visit_columns` text NOT NULL,
+  `unset_link_visit_action_columns` text NOT NULL,
+  `output` mediumtext,
+  `scheduled_date` datetime DEFAULT NULL,
+  `job_start_date` datetime DEFAULT NULL,
+  `job_finish_date` datetime DEFAULT NULL,
+  `requester` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`idlogdata_anonymization`),
+  KEY `job_start_date` (`job_start_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_report`
+--
+
+DROP TABLE IF EXISTS `matomo_report`;
+CREATE TABLE IF NOT EXISTS `matomo_report` (
+  `idreport` int(11) NOT NULL AUTO_INCREMENT,
+  `idsite` int(11) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `idsegment` int(11) DEFAULT NULL,
+  `period` varchar(10) NOT NULL,
+  `hour` tinyint(4) NOT NULL DEFAULT '0',
+  `type` varchar(10) NOT NULL,
+  `format` varchar(10) NOT NULL,
+  `reports` text NOT NULL,
+  `parameters` text,
+  `ts_created` timestamp NULL DEFAULT NULL,
+  `ts_last_sent` timestamp NULL DEFAULT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idreport`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_segment`
+--
+
+DROP TABLE IF EXISTS `matomo_segment`;
+CREATE TABLE IF NOT EXISTS `matomo_segment` (
+  `idsegment` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `definition` text NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `enable_all_users` tinyint(4) NOT NULL DEFAULT '0',
+  `enable_only_idsite` int(11) DEFAULT NULL,
+  `auto_archive` tinyint(4) NOT NULL DEFAULT '0',
+  `ts_created` timestamp NULL DEFAULT NULL,
+  `ts_last_edit` timestamp NULL DEFAULT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idsegment`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_sequence`
+--
+
+DROP TABLE IF EXISTS `matomo_sequence`;
+CREATE TABLE IF NOT EXISTS `matomo_sequence` (
+  `name` varchar(120) NOT NULL,
+  `value` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_session`
+--
+
+DROP TABLE IF EXISTS `matomo_session`;
+CREATE TABLE IF NOT EXISTS `matomo_session` (
+  `id` varchar(255) NOT NULL,
+  `modified` int(11) DEFAULT NULL,
+  `lifetime` int(11) DEFAULT NULL,
+  `data` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_site`
+--
+
+DROP TABLE IF EXISTS `matomo_site`;
+CREATE TABLE IF NOT EXISTS `matomo_site` (
+  `idsite` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(90) NOT NULL,
+  `main_url` varchar(255) NOT NULL,
+  `ts_created` timestamp NULL DEFAULT NULL,
+  `ecommerce` tinyint(4) DEFAULT '0',
+  `sitesearch` tinyint(4) DEFAULT '1',
+  `sitesearch_keyword_parameters` text NOT NULL,
+  `sitesearch_category_parameters` text NOT NULL,
+  `timezone` varchar(50) NOT NULL,
+  `currency` char(3) NOT NULL,
+  `exclude_unknown_urls` tinyint(1) DEFAULT '0',
+  `excluded_ips` text NOT NULL,
+  `excluded_parameters` text NOT NULL,
+  `excluded_user_agents` text NOT NULL,
+  `group` varchar(250) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `keep_url_fragment` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idsite`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_site_setting`
+--
+
+DROP TABLE IF EXISTS `matomo_site_setting`;
+CREATE TABLE IF NOT EXISTS `matomo_site_setting` (
+  `idsite` int(10) unsigned NOT NULL,
+  `plugin_name` varchar(60) NOT NULL,
+  `setting_name` varchar(255) NOT NULL,
+  `setting_value` longtext NOT NULL,
+  `json_encoded` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  KEY `idsite` (`idsite`,`plugin_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_site_url`
+--
+
+DROP TABLE IF EXISTS `matomo_site_url`;
+CREATE TABLE IF NOT EXISTS `matomo_site_url` (
+  `idsite` int(10) unsigned NOT NULL,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`idsite`,`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_user`
+--
+
+DROP TABLE IF EXISTS `matomo_user`;
+CREATE TABLE IF NOT EXISTS `matomo_user` (
+  `login` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `alias` varchar(45) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `token_auth` char(32) NOT NULL,
+  `superuser_access` tinyint(2) unsigned NOT NULL DEFAULT '0',
+  `date_registered` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`login`),
+  UNIQUE KEY `uniq_keytoken` (`token_auth`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_user_dashboard`
+--
+
+DROP TABLE IF EXISTS `matomo_user_dashboard`;
+CREATE TABLE IF NOT EXISTS `matomo_user_dashboard` (
+  `login` varchar(100) NOT NULL,
+  `iddashboard` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `layout` text NOT NULL,
+  PRIMARY KEY (`login`,`iddashboard`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matomo_user_language`
+--
+
+DROP TABLE IF EXISTS `matomo_user_language`;
+CREATE TABLE IF NOT EXISTS `matomo_user_language` (
+  `login` varchar(100) NOT NULL,
+  `language` varchar(10) NOT NULL,
+  `use_12_hour_clock` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
